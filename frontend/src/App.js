8937e2b043a63c3f507ad9068945c75ef2dd7636
OumaBon <mugahboniface@gmail.com>
2024-02-22 19:30:42 +0300

    initial frontend app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import { Header } from "./components/Header";
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Image</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './css/index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <div>
+  <h1>Welcome ot our page </h1>
+  <App/>
+ </div>
+);
+
+
