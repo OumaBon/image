7dbd99df7f99cc40c618c3aa8b9622c02647fab8
BonOuma <mcouma.mugah@gmail.com>
2024-02-22 23:53:46 +0300

    flask entry file

---
+++ Backend/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+app =Flask(__name__)
+
+
+@app.route("/",methods=["GET"])
+def root ():
+    return "<h1>Welcome to Our Page<\h1>"
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
